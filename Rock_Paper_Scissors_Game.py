f53c0244d16bba261493aff566541147f90b87f3
Erlisja <133474314+Erlisja@users.noreply.github.com>
2024-05-13 10:40:04 -0400

    modified:   Rock_Paper_Scissors_Game.py

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -41,43 +41,19 @@ while True:
         print("Computer:")
         print(options [computer])
     if (computer == player):
-        # print(f"Computer \n {options[computer]}")
-        # print(f"Player \n {options [player]}")
         print("It's a draw!")
     elif (computer == 0 and player == 1):
-        # print("Computer \n" + rock)
-        # print("Player \n" + paper)
         print ("You Won!")
     elif(computer == 0 and player == 2):
-        # print("Computer \n" + rock)
-        # print("Player \n" + scissors)
         print ("You Lost!")
     elif(computer == 1 and player == 0):
-        # print("Computer \n" + paper)
-        # print("Player \n" + rock)
         print ("You Lost!")
     elif(computer == 1 and player == 2):
-        # print("Computer \n" + paper)
-        # print("Player \n" + scissors)
         print ("You Won!")
     elif(computer == 2 and player == 0):
-        # print("Computer \n" + scissors)
-        # print("Player \n" + rock)
         print ("You Won!")
     elif(computer == 2 and player == 1):
-        # print("Computer \n" + scissors)
-        # print("Player \n" + paper)
         print ("You Lost!")
-    # if player == 0 and computer == 2:
-    #     print("You win!")
-    # elif computer == 0 and player == 2:
-    #     print("You lose")
-    # elif computer > player:
-    #     print("You lose")
-    # elif player > computer:
-    #     print("You win!")
-    # elif computer == player:
-    #     print("It's a draw")
 
     play_again = input('Do you want to play again? Type "Yes" or "No"\n').lower()
     if play_again != "yes":
